fb100f4b337670649c9055aa781600a422a75f2e
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-08 16:29:39 +0100

    feat: creazione della lista

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 "use.strict";
 
+// ?elementi HTML
+const elementLista = document.querySelector(".list");
+
+// ?variabili
+const divisibilePerTre = "Fizz";
+const divisibilePerCinque = "Buzz";
+
 for (let i = 1; i <= 100; i++) {
+  //* creazione elemento html li
+  const li = document.createElement("li");
+
+  //! condizioni
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    console.log(divisibilePerTre + divisibilePerCinque);
+    li.append(divisibilePerTre + divisibilePerCinque);
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    console.log(divisibilePerCinque);
+    li.append(divisibilePerCinque);
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    console.log(divisibilePerTre);
+    li.append(divisibilePerTre);
   } else {
     console.log({ i });
+    li.append(i);
   }
+
+  //* inserirsco li nella ul
+  elementLista.append(li);
 }
