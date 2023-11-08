578dd2f250cc3cdcf3bfa3142da296eeee5d91c4
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-08 17:34:03 +0100

    feat: diversi colori per i numeri

--- js/main.js
+++ js/main.js
@@ -11,20 +11,26 @@ for (let i = 1; i <= 100; i++) {
   //* creazione elemento html li
   const li = document.createElement("div");
   li.classList.add("-lc-list-item");
+  li.classList.add("rounded");
+  li.classList.add("shadow-lg");
 
   //! condizioni
   if (i % 3 === 0 && i % 5 === 0) {
     console.log(divisibilePerTre + divisibilePerCinque);
     li.append(divisibilePerTre + divisibilePerCinque);
+    li.classList.add("-lc-number-3-5");
   } else if (i % 5 === 0) {
     console.log(divisibilePerCinque);
     li.append(divisibilePerCinque);
+    li.classList.add("-lc-number-5");
   } else if (i % 3 === 0) {
     console.log(divisibilePerTre);
     li.append(divisibilePerTre);
+    li.classList.add("-lc-number-3");
   } else {
     console.log({ i });
     li.append(i);
+    li.classList.add("-lc-number");
   }
 
   //* inserirsco li nella ul
